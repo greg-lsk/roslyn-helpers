c2d7bc7ac6fd8f21e33c5b3213c926716166425b
Greg Liaskas <greg.liaskas@outlook.com>
2025-11-14 10:22:24 +0200

    test:: for DiagnosticDescriptor creation

---
+++ RoslynDiagnosticHelpers.Tests/Descriptor/DescriptorCreationTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+using RoslynHelpers.Descriptor;
+using static RoslynHelpers.Tests.Descriptor.TestData.DescriptorTestData;
+
+
+namespace RoslynHelpers.Tests.Descriptor;
+
+public class DescriptorCreationTests
+{
+    [Fact]
+    public void DiagnosticDescriptorHelper_CreatesDiagnosticDescriptor()
+    {
+        var created = DiagnosticDescriptorHelper.Create<UnlocalizableDescriptor>();
+        Assert.IsType<DiagnosticDescriptor>(created);
+    }
+}
---
+++ RoslynDiagnosticHelpers.Tests/Descriptor/TestData/DescriptorTestData.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.CodeAnalysis;
+using RoslynHelpers.Descriptor;
+
+namespace RoslynHelpers.Tests.Descriptor.TestData;
+
+internal class DescriptorTestData
+{
+    internal readonly struct UnlocalizableDescriptor : IDiagnosticDescriptor
+    {
+        public readonly DiagnosticDescriptor Create() => new
+        (
+            id: "Id", 
+            title: "Title", 
+            messageFormat: "Message", 
+            category: "Category", 
+            defaultSeverity: DiagnosticSeverity.Warning, 
+            isEnabledByDefault: true
+        );
+    }
+}

fcfe6e69be49839fa2e79d283ad495c611117129
Greg Liaskas <greg.liaskas@outlook.com>
2025-11-14 11:43:36 +0200

    feat:: logic for instantiating the LocalizableResourceString class

---
+++ RoslynDiagnosticHelpers/LocalizableResource/LocalizableStringHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Resources;
+using Microsoft.CodeAnalysis;
+
+
+namespace RoslynHelpers.LocalizableResource;
+
+public static class LocalizableStringHelper
+{
+    public static LocalizableString From<TResource>(string resourceName, ResourceManager resourceManager)
+    {
+        return new LocalizableResourceString(nameof(resourceName), resourceManager, typeof(TResource));
+    }
+
+    public static LocalizableString From<TResource>(string resourceName, ResourceManager resourceManager, params string[] formatArguments)
+    {
+        return new LocalizableResourceString(nameof(resourceName), resourceManager, typeof(TResource), formatArguments);
+    }
+}

8b788c2e5d497f061e5ae3e24d02ce7d04a2fb38
Greg Liaskas <greg.liaskas@outlook.com>
2025-11-14 13:35:32 +0200

    test:: for LocalizableString creation

---
+++ RoslynDiagnosticHelpers.Tests/LocalizableResource/LocalizableStringCreationTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.CodeAnalysis;
+using RoslynHelpers.LocalizableResource;
+using RoslynHelpers.Tests.LocalizableResource.TestData;
+
+
+namespace RoslynHelpers.Tests.LocalizableResource;
+
+public class LocalizableStringCreationTests
+{
+    [Fact]
+    public void LocalizableStringHelper_CreatesLocalizableResourceString()
+    {
+        var withNoFormat = LocalizableStringHelper.From<TestResources>
+        (
+            TestResources.TestLocalizableResource,
+            TestResources.ResourceManager
+        );
+
+        var withFormat = LocalizableStringHelper.From<TestResources>
+        (
+            TestResources.TestLocalizableResource,
+            TestResources.ResourceManager,
+            ["a", "ddd"]
+        );
+
+        Assert.IsType<LocalizableResourceString>(withNoFormat);
+        Assert.IsType<LocalizableResourceString>(withFormat);
+    }
+
+    [Fact]
+    public void LocalizableStringHelper_Creation_YieldsSameResultAs_LocalizableResourceStringCtor()
+    {
+        var withNoFormatFromHelper = LocalizableStringHelper.From<TestResources>
+        (
+            TestResources.TestLocalizableResource,
+            TestResources.ResourceManager
+        );
+        var withNoFormatFromCtor = new LocalizableResourceString
+        (
+            TestResources.TestLocalizableResource,
+            TestResources.ResourceManager,
+            typeof(TestResources)
+        );
+
+
+        var withFormatFromHelper = LocalizableStringHelper.From<TestResources>
+        (
+            TestResources.TestLocalizableResource,
+            TestResources.ResourceManager,
+            ["a", "ddd"]
+        );
+        var withFormatFromCtor = new LocalizableResourceString
+        (
+            TestResources.TestLocalizableResource,
+            TestResources.ResourceManager,
+            typeof(TestResources),
+            ["a", "ddd"]
+        );
+
+
+        Assert.True(withNoFormatFromHelper.Equals(withNoFormatFromCtor));
+        Assert.True(withFormatFromHelper.Equals(withFormatFromCtor));
+    }
+}
